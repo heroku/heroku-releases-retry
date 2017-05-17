a37740719326abc888ccd09ede5d6cd771071bb3
dmathieu <42@dmathieu.com>
2017-05-17 10:45:05 +0200

    display the release command output if it is available
    tell the customer to retry with `releases:output` if we got a 404
    
    It can take a few seconds before the stream is actually available. This
    message is there until we fix the issue with retries.
    
    set the retry variable by equally directly from yield
    
    don't overcomplicate things with a non-reused lib file

--- test/commands/retry.js
+++ test/commands/retry.js
@@ -4,6 +4,8 @@
 const cli = require('heroku-cli-util')
 const nock = require('nock')
 const cmd = require('../..').commands.find(c => c.topic === 'releases' && c.command === 'retry')
+const expect = require('chai').expect
+const stdMocks = require('std-mocks')
 
 describe('releases:retry', function () {
   beforeEach(() => cli.mockConsole())
@@ -16,5 +18,42 @@ describe('releases:retry', function () {
       .reply(200, {})
     return cmd.run({app: 'myapp'})
       .then(() => api.done())
+  })
+
+  it('retries the release with output', function () {
+    stdMocks.use()
+    let busl = nock('https://busl.test:443')
+      .get('/streams/release.log')
+      .reply(200, 'Release Output Content')
+    let api = nock('https://api.heroku.com:443')
+      .get('/apps/myapp/releases')
+      .reply(200, [{ 'slug': { id: 'slug_uuid' }, version: 40 }])
+      .post('/apps/myapp/releases', { slug: 'slug_uuid', description: 'Retrying v40' })
+      .reply(200, {output_stream_url: 'https://busl.test/streams/release.log'})
+
+    return cmd.run({app: 'myapp'})
+      .then(() => expect(stdMocks.flush().stdout.join('')).to.equal('Release Output Content'))
+      .then(() => expect(cli.stderr).to.equal(''))
+      .then(() => api.done())
+      .then(() => busl.done())
+      .then(() => stdMocks.restore())
+      .catch(() => stdMocks.restore())
+  })
+
+  it('retries the release with missing output', function () {
+    let busl = nock('https://busl.test:443')
+      .get('/streams/release.log')
+      .reply(404, '')
+    let api = nock('https://api.heroku.com:443')
+      .get('/apps/myapp/releases')
+      .reply(200, [{ 'slug': { id: 'slug_uuid' }, version: 40 }])
+      .post('/apps/myapp/releases', { slug: 'slug_uuid', description: 'Retrying v40' })
+      .reply(200, {version: 1, output_stream_url: 'https://busl.test/streams/release.log'})
+
+    return cmd.run({app: 'myapp'})
+      .then(() => expect(cli.stdout).to.equal('Running release command...\n'))
+      .then(() => expect(cli.stderr).to.contain('Release command starting. Use `heroku releases:output` to view the log.\n'))
+      .then(() => api.done())
+      .then(() => busl.done())
   })
 })
